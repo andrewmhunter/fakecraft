05de9dc01448b79787b5678a165d303a836f4fdc
andrewmhunter <sockymanthesock@gmail.com>
2026-06-29 16:21:14 -0400

    Removed old, commented out world code.

--- src/world.hpp
+++ src/world.hpp
@@ -48,22 +48,6 @@ public:
     }
 };
 
-/*
-void worldInit(World* world);
-void worldUnload(World* world);
-void worldUpdate(World* world, float deltaTime);
-void worldDraw(World* world, ShaderProgram& terrainShader, ShaderProgram& entityShader);
-Block worldGetBlock(const World* world, glm::ivec3 worldPoint);
-void worldSetBlock(World* world, glm::ivec3 worldPoint, Block block);
-Chunk* worldGetChunk(World* world, glm::ivec3 chunkCoords);
-const Chunk* worldGetChunkConst(const World* world, glm::ivec3 chunkCoords);
-
-void worldMarkDirty(World* world, glm::ivec3 worldPoint);
-void worldTryPlaceBlock(World* world, glm::ivec3 worldPoint, Block block);
-void worldTryPlaceBox(World* world, glm::ivec3 start, glm::ivec3 end, Block block);
-void worldPlaceBox(World* world, glm::ivec3 start, glm::ivec3 end, Block block);
-*/
-
 extern int shaderModelUniform;
 extern int shaderSkylight;
 extern int shaderFogColor;
